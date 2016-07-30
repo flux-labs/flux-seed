b153c1b1bd95344ae55a6193c62f999a05004806
karlgarske <karlgarske@MacBook-Pro.local>
2016-07-30 12:57:24 -0700

    Removed obsolete chapters

--- tutorials/chapter_4_read_more/js/config.js
+++
@@ -1,5 +0,0 @@
-let config = {
-  url: 'http://localhost:8080', // your url
-  flux_url: 'https://flux.io', // flux url
-  flux_client_id: '24360f17-83ce-4b11-98a0-b51149896955', // your app's client id
-}
